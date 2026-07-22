44c8346943b48d4c159736d13aac3c857aafda64
Chris Zheng <zcaudate@outlook.com>
2026-07-23 04:19:36 +1000

    Add the first Hara core bootstrap resource

--- src/test/java/hara/truffle/HaraLanguageTest.java
+++ src/test/java/hara/truffle/HaraLanguageTest.java
@@ -27,6 +27,18 @@ public class HaraLanguageTest {
   }
 
   @Test
+  public void loadsTheLanguageLevelCoreBootstrapResource() {
+    try (Context context = context()) {
+      assertEquals(
+          42,
+          context
+              .eval(HaraLanguage.ID, "(load-resource \"hara/l0-core.hara\") ((comp2 inc inc) 40)")
+              .asLong());
+      assertEquals(9, context.eval(HaraLanguage.ID, "((constantly 9) nil)").asLong());
+    }
+  }
+
+  @Test
   public void dispatchesDefmultiAndDefmethodByArbitraryValues() {
     try (Context context = context()) {
       context.eval(HaraLanguage.ID, "(defmulti kind (fn [x] (if (= x 1) :one :other)))");
